afdfb01398b24cbe576e7a0ab08e6b2c5f9261b3
Daniel <danieldfcreis@gmail.com>
2025-10-11 13:04:05 -0300

    aprendendo python

--- ex001.py
+++ ex001.py
@@ -1,2 +1,7 @@
-nome = input('Digite seu nome: ')
-print('Prazer em te conhecer!', nome)
+nome =input('Digite seu nome: ')
+num1 =int(input('Me fale um numero: '))
+num2 =int(input('Me fale um numero: '))
+soma = num1 + num2
+#print('Ola',nome,'a soma dos numeros',num1,'e',num2,'=',soma)
+print('A soma entre os numeros {} e {} e igual a {}'.format(num1, num2, soma))
+
---
+++ ex003.py
@@ -0,0 +1,7 @@
+
+n = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+soma = n+ n2
+print('A soma dos valores {} e {} e igual a {}'.format(n, n2 ,soma))
+n3 = input('Digite alguma coisa: ')
+print(n3.isalnum())
