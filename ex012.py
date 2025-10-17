416b07648d271101bdf2d8f2f8f725147dee3c58
Daniel <danieldfcreis@gmail.com>
2025-10-17 15:32:39 -0300

    adicionando bibliotecas externas

--- ex012.py
+++ ex012.py
@@ -1,7 +1,7 @@
 '''Calculando Hipotenusa'''
 from math import hypot
 print('Vamos calcular o valor da Hipotenusa de um triangulo retangulo')
-a = int(input('Me fale o valor do primeiro cateto? '))
-b = int(input('Me fale o valor do segundo cateto? '))
+a = int(input('Me fale o valor do cateto oposto? '))
+b = int(input('Me fale o valor do cateto adjacente? '))
 c = hypot(a, b)
 print('A soma hipotenusa dos catetors {} e {} e igual a {}'.format(a, b ,c))
